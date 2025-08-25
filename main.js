e902a0f959aa2413e644bc15af07616d3fe89b5c
hemingus <heming.hanevik@gmail.com>
2025-08-25 12:51:10 +0200

    update

---
+++ main.js
@@ -0,0 +1,55 @@
+console.log("Hello world!")
+const handleListe = localStorage.getItem("handleListe")
+const formHandleliste = document.getElementById("form")
+const containerHandleliste = document.getElementById("container")
+
+function saveList(list) {
+    localStorage.setItem("handleListe", JSON.stringify(handleListe))
+}
+
+function renderHandleliste() {
+    container.innerHTML = ""
+    handleListe.forEach((x, index) => {
+        const p = document.createElement("p")
+        p.textContent = `${x.navn} ${x.pris} kr (${sunnToString(x.sunn)})`
+        const btn = document.createElement("button")
+        btn.innerHTML = "remove"
+        btn.addEventListener("click", () => {
+            handleListe.splice(index, 1)
+            renderHandleliste();
+        })
+        p.appendChild(btn);
+        container.appendChild(p);
+        saveList(handleListe);
+    })
+    
+}
+
+formHandleliste.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const navn = document.getElementById("input-navn").value;
+    const pris = document.getElementById("input-pris").value;
+    const sunn = document.getElementById("input-sunn").checked;
+
+    handleListe.push({navn, pris, sunn})
+    formHandleliste.reset()
+    renderHandleliste()
+
+})
+
+function sunnToString(bool) {
+    let str = ""
+    bool === true ? str = "sunn" : str = "usunn"
+    return str
+}
+
+renderHandleliste()
+
+
+
+
+
+
+
+
+    
